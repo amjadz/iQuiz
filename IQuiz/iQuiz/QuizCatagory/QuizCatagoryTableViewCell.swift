c7a1110472e1c79aa06d4ff145c7480d9fbb27ce
Zubair Amjad <amjadz@uw.edu>
2018-10-27 15:18:49 -0700

    Code is one gh

---
+++ IQuiz/iQuiz/QuizCatagory/QuizCatagoryTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuizCatagoryTableViewCell.swift
+//  iQuiz
+//
+//  Created by Zubair Amjad on 10/27/18.
+//  Copyright © 2018 Zubair Amjad. All rights reserved.
+//
+
+import UIKit
+
+class QuizCatagoryTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
